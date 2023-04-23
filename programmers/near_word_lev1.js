cafe83206849e4dacd59b435b26cfbfea5d08008
Minseok Chae <117037249+chaeminseok@users.noreply.github.com>
2023-04-23 23:55:31 +0900

    가장 가까운 같은 글자

---
+++ programmers/near_word_lev1.js
@@ -0,0 +1,17 @@
+const solution = (s) => {
+  let stack = [];
+  let ans = [];
+
+  [...s].forEach((str) => {
+    if (!stack.includes(str)) {
+      stack.push(str);
+      ans.push(-1);
+    } else {
+      ans.push(stack.length - stack.lastIndexOf(str));
+      stack.push(str);
+    }
+  });
+
+  return ans;
+};
+console.log(solution("banana"));
