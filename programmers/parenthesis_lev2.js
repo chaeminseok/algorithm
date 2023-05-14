abb0ae7e1699095288542878b2099627eb5ec7dd
Minseok Chae <117037249+chaeminseok@users.noreply.github.com>
2023-05-14 23:59:14 +0900

    console

--- programmers/parenthesis_lev2.js
+++ programmers/parenthesis_lev2.js
@@ -1,25 +1,3 @@
-// function solution(s) {
-//   var answer = true;
-//   let stack = 0;
-//   s = s.split("");
-//   for (let i = 0; i < s.length; i++) {
-//     if (s[i] === "(") {
-//       stack += 1;
-//     } else if (s[i] === ")") {
-//       stack -= 1;
-//     }
-//     if (stack < 0) {
-//       answer = false;
-//       break;
-//     } else if (i === s.length - 1 && stack !== 0) {
-//       answer = false;
-//       break;
-//     } else if (i === s.length - 1 && stack === 0) {
-//       answer = true;
-//     }
-//   }
-//   return answer;
-// }
 function solution(s) {
   let cum = 0;
   for (let paren of s) {
@@ -30,4 +8,4 @@ function solution(s) {
   }
   return cum === 0 ? true : false;
 }
-//console.log(solution("(())()"));
+console.log(solution("(())()"));
