b278427685112273a37089124382205ff65a7111
Minseok Chae <117037249+chaeminseok@users.noreply.github.com>
2023-05-18 23:59:21 +0900

    12

---
+++ programmers/oxquiz_lev0.js
@@ -0,0 +1,10 @@
+function solution(quiz) {
+  var answer = [];
+  var number;
+  for (i of quiz) {
+    number = i.split(" ");
+  }
+
+  return answer;
+}
+console.log(solution(["3 - 4 = -3", "5 + 6 = 11"]));
