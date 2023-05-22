6b96243dc66e7edd6640c829d432c33e88f4a1a0
Minseok Chae <117037249+chaeminseok@users.noreply.github.com>
2023-05-22 23:50:04 +0900

    배열의 길이의 따라 다른 연산하기

--- programmers/different_array_length_lev0.js
+++ programmers/different_array_length_lev0.js
@@ -1,8 +1,4 @@
-function solution(arr, n) {
-    var answer = [];
-   if(arr.length%2===1){
-        return answer = arr.map((item)=>{
-            if(item)
-        })  
-   }
-}
+const solution = (arr, n) => {
+  return arr.map((num, idx) => (arr.length % 2 !== idx % 2 ? num + n : num));
+};
+console.log(solution([49, 12, 100, 276, 33], 27));
