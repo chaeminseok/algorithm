c77489f443fd71c2e750fb596cdbb551967b47ad
Minseok Chae <117037249+chaeminseok@users.noreply.github.com>
2023-06-04 23:56:58 +0900

    부족한금액계산하기

---
+++ programmers/부족한금핵계산하기_lev1.js
@@ -0,0 +1,10 @@
+const solution = (price, money, count) => {
+  var answer = 0;
+  let totalPrice = 0;
+  for (let i = 1; i <= count; i++) {
+    answer += price * i;
+  }
+
+  return answer > money ? answer - money : 0;
+};
+console.log(solution(3, 20, 4));
