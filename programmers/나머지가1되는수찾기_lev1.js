8de8ea2cea5620885b1647adccc3b3103154363f
Minseok Chae <117037249+chaeminseok@users.noreply.github.com>
2023-06-09 23:44:00 +0900

    나머지가 1되는수기

---
+++ programmers/나머지가1되는수찾기_lev1.js
@@ -0,0 +1,14 @@
+function solution(n) {
+  var answer = 0;
+
+  if (n % 2 === 1) {
+    return 2;
+  } else {
+    for (let i = 3; i < n; i++) {
+      if (n % i === 1) {
+        return i;
+        break;
+      }
+    }
+  }
+}
