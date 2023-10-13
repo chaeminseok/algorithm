6fc790bc67934fce76e81c64803a3a1380e55b75
chaeminseok <minseok23491@naver.com>
2023-10-13 23:58:33 +0900

    Create 햄버거만들기_lev1.js

---
+++ programmers/햄버거만들기_lev1.js
@@ -0,0 +1,18 @@
+function solution(ingredient) {
+  let cnt = 0;
+  for (let i = 3; i < ingredient.length; i += 1) {
+    const now = ingredient[i];
+    if (
+      now === 1 &&
+      ingredient[i - 1] === 3 &&
+      ingredient[i - 2] === 2 &&
+      ingredient[i - 3] === 1
+    ) {
+      ingredient.splice(i - 3, 4);
+      cnt += 1;
+      i -= 4;
+    }
+  }
+  return cnt;
+}
+console.log(solution([2, 1, 1, 2, 3, 1, 2, 3, 1]));
